ca089ad1db9893f635c3d01191e0804c1ec21f31
Greg Sabella <gsabella4@gmail.com>
2023-01-30 20:05:13 -0500

    pushing changes to schema.sql

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -18,15 +18,15 @@ CREATE TABLE users (
 
 CREATE TABLE user_information (
     -- current PK is username, is it better practice to have a Serial id for this table? as PK
+    -- account_id here as pk?
 	username varchar(50) NOT NULL,
 	--full_name or first_name & last_name? What works best with our form data?
 	full_name varchar(100) NOT NULL,
-	street_address varchar(100) NOT NULL,
-	city varchar(50) NOT NULL,
-	/* full state name or state abbreviation*/
-	state_name varchar(15) NOT NULL,
-	zipcode char(15) NOT NULL,
-	phone_number char(10), -- example: 7032978845
+	street_address varchar(100) NOT NULL, -- 3001 Railroad St
+	city varchar(50) NOT NULL, -- Pittsburgh
+	state_abbreviation varchar(2) NOT NULL, -- PA
+	zipcode varchar(5) NOT NULL, -- 15201
+	phone_number varchar(11), -- example: 7032978845 (country code '1' optional)
 	email_address varchar(100),
 	CONSTRAINT PK_user_information PRIMARY KEY (username),
 	CONSTRAINT FK_user_information_users FOREIGN KEY (username) REFERENCES users (username)
@@ -35,9 +35,9 @@ CREATE TABLE user_information (
 CREATE TABLE recycle_records (
 	account_id SERIAL, -- added serial account_id as PK for records table
 	username varchar(50) NOT NULL,
-	total_pounds_recycled numeric(10) DEFAULT 0,
-	credit_points numeric(10) DEFAULT 0,
-	points_redeemed numeric(10) DEFAULT 0,
+	total_pounds_recycled int DEFAULT 0,
+	credit_points int DEFAULT 0,
+	points_redeemed int DEFAULT 0,
 	CONSTRAINT PK_recycle_records PRIMARY KEY (account_id),
 	CONSTRAINT FK_recycle_records_user_information FOREIGN KEY (username) REFERENCES user_information (username)
 );
@@ -48,11 +48,11 @@ CREATE TABLE pickup_information (
 	route_id int,
 	username varchar(50) NOT NULL,
 	place_id varchar(200) NOT NULL, -- for Route API use, is varchar(200) workable?
-	pickup_date date,
-	pickup_time time,
+	pickup_date date NOT NULL,
+	-- pickup_time time -- optional: unless we need time for scheduling, instead of just date (route is organized by efficiency)
 	--Another option would be: pickup_date_time TIMESTAMP (NOT NULL), What works best? as the data will be coming from the schedule pickup form*/
-	pickup_weight numeric(3), -- full weight * num_of_bins
-	num_of_bins numeric(1), -- limit number to 3 bins.
+	pickup_weight int NOT NULL, -- full weight * num_of_bins
+	num_of_bins int NOT NULL, -- limit number to 3 bins.
 	isPickedUp boolean DEFAULT false,
 	CONSTRAINT PK_pickup_information PRIMARY KEY (pickup_id),
     CONSTRAINT FK_pickup_information_users FOREIGN KEY (driver_id) REFERENCES users (user_id),
@@ -61,7 +61,7 @@ CREATE TABLE pickup_information (
 );
 
 
--- Thinking consolidation of pickups per route and driver assigned.
+-- Thinking consolidation of pickups per route and driver assigned. Reflecting pickups assigned to driver
 --CREATE TABLE routes (
 --    route_id SERIAL,
 --    driver_id int,
