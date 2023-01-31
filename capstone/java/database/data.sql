55e6fe5a870d558f880d879f7be4b85ce3dc84b2
Greg Sabella <gsabella4@gmail.com>
2023-01-30 22:40:44 -0500

    pushing updates to db design 'schema.sql'

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -1,7 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role,is_driver) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', default);
-INSERT INTO users (username,password_hash,role,is_driver) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', default);
-INSERT INTO users (username,password_hash,role,is_driver) VALUES ('driver_test','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', true); --loading driver, testing is_driver boolean
+INSERT INTO users (username,password_hash,role, is_driver) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', false);
+INSERT INTO users (username,password_hash,role, is_driver) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', false);
 
 COMMIT TRANSACTION;
--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,26 +1,34 @@
 BEGIN TRANSACTION;
 
---DROP TABLE IF EXISTS routes;
-DROP TABLE IF EXISTS recycle_records;
-DROP TABLE IF EXISTS pickup_information;
-DROP TABLE IF EXISTS user_information;
+DROP TABLE IF EXISTS pickup_details;
+DROP TABLE IF EXISTS driver_details;
+DROP TABLE IF EXISTS user_details;
 DROP TABLE IF EXISTS users;
 
+DROP SEQUENCE IF EXISTS seq_account_id, seq_employee_id;
+
+-- user/password information for users who recycle, drivers, and admins
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
-    is_driver boolean DEFAULT false,
+	is_driver boolean DEFAULT false,
 	CONSTRAINT PK_user PRIMARY KEY (user_id),
 	CONSTRAINT UQ_username UNIQUE (username)
 );
 
-CREATE TABLE user_information (
-    -- current PK is username, is it better practice to have a Serial id for this table? as PK
-    -- account_id here as pk?
+-- User information for those registered to recycle with us
+-- Sequence below --> recyclers have account_id starting with 1001
+CREATE SEQUENCE seq_account_id
+    INCREMENT BY 1
+    START WITH 1001
+    NO MAXVALUE;
+
+CREATE TABLE user_details (
+    account_id int NOT NULL DEFAULT nextval ('seq_account_id'),
 	username varchar(50) NOT NULL,
-	--full_name or first_name & last_name? What works best with our form data?
+	--full_name or first_name & last_name? does one or the other benefit us when user inputs data on the front end?
 	full_name varchar(100) NOT NULL,
 	street_address varchar(100) NOT NULL, -- 3001 Railroad St
 	city varchar(50) NOT NULL, -- Pittsburgh
@@ -28,47 +36,45 @@ CREATE TABLE user_information (
 	zipcode varchar(5) NOT NULL, -- 15201
 	phone_number varchar(11), -- example: 7032978845 (country code '1' optional)
 	email_address varchar(100),
-	CONSTRAINT PK_user_information PRIMARY KEY (username),
-	CONSTRAINT FK_user_information_users FOREIGN KEY (username) REFERENCES users (username)
-);
-
-CREATE TABLE recycle_records (
-	account_id SERIAL, -- added serial account_id as PK for records table
-	username varchar(50) NOT NULL,
 	total_pounds_recycled int DEFAULT 0,
-	credit_points int DEFAULT 0,
-	points_redeemed int DEFAULT 0,
-	CONSTRAINT PK_recycle_records PRIMARY KEY (account_id),
-	CONSTRAINT FK_recycle_records_user_information FOREIGN KEY (username) REFERENCES user_information (username)
+    credits_balance int DEFAULT 0,
+    credits_redeemed int DEFAULT 0,
+	CONSTRAINT PK_user_details PRIMARY KEY (username),
+	CONSTRAINT FK_user_details_users FOREIGN KEY (username) REFERENCES users (username)
 );
 
-CREATE TABLE pickup_information (
+-- Driver information
+-- Sequence below --> Drivers have employee_id, starting with 3001
+CREATE SEQUENCE seq_employee_id
+    INCREMENT BY 1
+    START WITH 3001
+    NO MAXVALUE;
+
+CREATE TABLE driver_details (
+    employee_id int NOT NULL DEFAULT nextval ('seq_employee_id'),
+    username varchar(50) NOT NULL,
+    home_office varchar(200) DEFAULT ('3001 Railroad St, Pittsburgh, PA 15201'), -- example. 3001 Railroad St, Pittsburgh, PA 15201 -- full address could avoid concatenation when feeding into API
+    CONSTRAINT PK_driver_details PRIMARY KEY (employee_id),
+    CONSTRAINT FK_driver_details_users FOREIGN KEY (username) REFERENCES users (username)
+);
+
+-- pickup information
+CREATE TABLE pickup_details (
 	pickup_id SERIAL,
 	driver_id int,
 	route_id int,
-	username varchar(50) NOT NULL,
-	place_id varchar(200) NOT NULL, -- for Route API use, is varchar(200) workable?
+	requesting_username varchar(50) NOT NULL,
 	pickup_date date NOT NULL,
 	-- pickup_time time -- optional: unless we need time for scheduling, instead of just date (route is organized by efficiency)
 	--Another option would be: pickup_date_time TIMESTAMP (NOT NULL), What works best? as the data will be coming from the schedule pickup form*/
 	pickup_weight int NOT NULL, -- full weight * num_of_bins
-	num_of_bins int NOT NULL, -- limit number to 3 bins.
-	isPickedUp boolean DEFAULT false,
-	CONSTRAINT PK_pickup_information PRIMARY KEY (pickup_id),
-    CONSTRAINT FK_pickup_information_users FOREIGN KEY (driver_id) REFERENCES users (user_id),
-    CONSTRAINT FK_pickup_information_user_information FOREIGN KEY (username) REFERENCES user_information (username),
-	CONSTRAINT chk_num_of_bins CHECK (num_of_bins <= 3)
+	num_of_bins int NOT NULL, -- User selection of 1, 2, or 3(max) per pickup
+	is_Picked_Up boolean DEFAULT false,
+	CONSTRAINT PK_pickup_details PRIMARY KEY (pickup_id),
+    CONSTRAINT FK_pickup_details_driver_details FOREIGN KEY (driver_id) REFERENCES driver_details (employee_id),
+    CONSTRAINT FK_pickup_details_user_details FOREIGN KEY (requesting_username) REFERENCES user_details (username),
+	CONSTRAINT chk_num_of_bins CHECK (num_of_bins > 0 AND num_of_bins <= 3)
 );
-
-
--- Thinking consolidation of pickups per route and driver assigned. Reflecting pickups assigned to driver
---CREATE TABLE routes (
---    route_id SERIAL,
---    driver_id int,
---    pickup_id int
---    CONSTRAINT PK_routes PRIMARY KEY (route_id),
---    CONSTRAINT FK_routes_pickup_information FOREIGN KEY (driver_id) REFERENCES pickup_information (driver_id),
---);
 
 
 
