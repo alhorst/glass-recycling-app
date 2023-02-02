8d7d3388b2cfb8f52a59f008299e04acba76914b
Alexis Horst <alexisl.horst@gmail.com>
2023-02-02 17:52:42 -0500

    added post, put, delete to pickupservice

--- capstone/vue/src/services/PickupService.js
+++ capstone/vue/src/services/PickupService.js
@@ -3,16 +3,28 @@ import axios from 'axios';
 export default {
 
     getPickupById(pickupId) {
-        return axios.get(`/pickups/${pickupId}`)
+        return axios.get(`/pickups/${pickupId}`);
     }, //remember to change this to http if we set a baseURL
 
     getPickups() {
-        return axios.get('/pickups')
+        return axios.get('/pickups');
     },
 
     getPickupByDriverId(driverId) {
-        return axios.get(`/pickups/${driverId}`)
+        return axios.get(`/pickups/${driverId}`);
     },
+
+    addPickup(pickup) {
+        return axios.post('/pickups', pickup);
+    },
+
+    updatePickup(pickup) {
+        return axios.put(`/pickups/${pickup.pickupId}`, pickup);
+    },
+
+    deletePickup(pickupId) {
+        return axios.delete(`/pickups/${pickupId}`);
+    }
 
 
 
