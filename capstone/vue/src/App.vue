26fd80c2ef0218e1f2e68aa5b35a43887d160478
Alexis Horst <alexisl.horst@gmail.com>
2023-02-03 22:02:04 -0500

    still working on css on app and home

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="app">
-    <div class="navbar">
-      <!-- Navbar -->
-      <div id="nav-container">
-        <img id="logo" src="./img/logo.png" alt="Vitrum Recycling Logo" />
-        <router-link v-bind:to="{ name: 'home' }"
-          ><i class="fa fa-fw fa-home"></i>Home</router-link
-        >
-        <router-link v-bind:to="{ name: 'account' }"
-          ><i class="fa fa-fw fa-user"></i>Account</router-link
-        >
-        <!-- This router link for logout is going to dropdown from Account...once I figure it out -->
-        <router-link
-          v-bind:to="{ name: 'logout' }"
-          v-if="$store.state.token != ''"
-          >Logout</router-link
-        >
-      </div>
+    <!-- Nav bar -->
+    <div class="nav-container">
+      <!-- <div id="navbar"> -->
+      <img id="logo" src="./img/logo-clear.png" alt="Vitrum Recycling Logo" />
+      <nav>
+        <ul>
+          <li>
+            <router-link v-bind:to="{ name: 'home' }"
+              ><i class="fa-solid fa-house"></i>Home</router-link
+            >
+          </li>
+          <li><i class="fa-solid fa-recycle"></i>Services</li>
+        </ul>
+      </nav>
+      <div class="spacer">&nbsp;</div>
+      <router-link v-bind:to="{ name: 'account' }"
+        >Account<i class="fa-solid fa-user"></i
+      ></router-link>
+      <!-- This router link for logout is going to dropdown from Account...once I figure it out -->
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
+      <!-- </div> -->
     </div>
     <router-view />
   </div>
@@ -25,9 +33,7 @@
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap");
 
-/* Why the heck is the gradient applying to ever box */
-#app {
-  background: rgb(11, 122, 30);
+html {
   background: linear-gradient(
     0deg,
     rgba(11, 122, 30, 1) 0%,
@@ -38,36 +44,70 @@
 
 #logo {
   float: left;
+  padding-left: 10px;
+  padding-top: 10px;
+  height: auto;
+  width: 15%;
 }
-#nav {
-  font-family: "Raleway", sans-serif;
-  font-size: 1.5em;
-  text-align: center;
+.nav-container {
+  border: 1px solid red;
   background-color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  font-size: 20px;
+  padding: 10px;
   width: 100%;
+  left: 0;
+  top: 0;
+  z-index: 1;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 }
 
-a:link,
-a:visited {
-  /* background-color: green; */
-  color: white; 
-  padding: 14px 20px;
-  text-align: center;
-  font-size: 1.2em;
+/* #navbar {
+  background-color: white;
+  padding: 0.2em;
+  width: 100%;
+} */
+
+ul {
+  display: flex;
+  justify-content: flex-start;
+  list-style-type: none;
+}
+
+li {
+  font-size: 20px;
+  font-weight: 600;
+  padding: 0 20px 0 0;
+  margin-left: 10px;
+  margin-top: 10px;
+  color: #0b7a1e;
+}
+
+i {
+  padding-right: 5px;
+  padding-left: 10px;
+}
+.spacer {
+  flex-grow: 1;
+}
+
+a {
+  box-shadow: inset 0 0 0 0 #0b7a1e;
+  color: #0b7a1e;
+  margin: 0 -0.25rem;
+  padding: 0 0.25rem;
+  transition: color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
   text-decoration: none;
-  display: inline-block;
+  text-align: center;
+  margin-right: 20px;
+  font-weight: 600;
 }
 
 a:hover {
-  /* background-color: blue; */
+  box-shadow: inset 200px 0 0 0 #9bdb66;
+  color: white;
 }
-a:active {
-  /* background-color: pink; */
-}
-/* .container {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-} */
 </style>
--- capstone/vue/src/views/Home.vue
+++ capstone/vue/src/views/Home.vue
@@ -3,19 +3,10 @@
 
 <template>
   <div id="container">
-    <div id="home" class="header">
-      <!-- <img
-        src="../img/image (1).png"
-        alt="Glass Recycling Logo"
-        class="logo"
-     
-      <section class="desc">Vitrum Recycle</section> /> -->
-    </div>
-
     <!--container for information and the sign up, registration" -->
     <div class="body-container">
-      <div class="first-container">
-        <div class="info">
+      <div class="about-us">
+        <div id="info">
           <b>VITRUM</b> Recycle is a grassroots program offering curbside glass
           recycling in the Pittsburgh metro region. Despite its status as an
           infinitely recyclable material, 75% of glass containers used by
@@ -23,21 +14,27 @@
           decompose.
           <!-- <img src="../img/bottles.png" alt="bottles" class="info-image" /> -->
         </div>
-        <div class="detail-image"> </div>
+        <div class="detail-image"></div>
       </div>
       <div class="mid-container">
         <div>
-          Enroll with Vitrum today and our fleet of glass wranglers will deliver custom bins to your home or business.
+          Enroll with Vitrum today and our fleet of glass wranglers will deliver
+          custom bins to your home or business.
         </div>
         <div></div>
       </div>
       <div class="last-container">
         <div></div>
-        <div>When those bins are full, simply schedule a pickup date on our handy app and drop those wine bottles 
-          and jam jars at the curb; they will be crushed to a fine sand which we sell or donate to our commercial 
-          partners for a wide variety of sustainable applications: erosion remediation, creating road services, 
-          and even to make swanky and unique kitchen countertops. Keep track of your recycling stats on your 
-          dashboard and earn points with every pickup to redeem for cool prizes. Sign up today!</div>
+        <div>
+          When those bins are full, simply schedule a pickup date on our handy
+          app and drop those wine bottles and jam jars at the curb; they will be
+          crushed to a fine sand which we sell or donate to our commercial
+          partners for a wide variety of sustainable applications: erosion
+          remediation, creating road services, and even to make swanky and
+          unique kitchen countertops. Keep track of your recycling stats on your
+          dashboard and earn points with every pickup to redeem for cool prizes.
+          Sign up today!
+        </div>
       </div>
       <div id="account">
         <nav id="sign-in" class="account-sign">
@@ -95,7 +92,6 @@
 </template>
 
 <script>
-
 export default {
   name: "home",
   //name: "account"
@@ -105,40 +101,23 @@ export default {
 
 <style scope>
 * {
-  font-family: 'Raleway', sans-serif;
+  font-family: "Raleway", sans-serif;
 }
-#container{
-  /* background-color: #bfe3bc; */
+#container {
+  border: 1px solid red;
 }
 
-#first-container{
+#first-container {
   display: flex;
-  
-  
-}
-.detail-image{
-  flex: 1;
-}
-
-.detail {
-  /* background-color: #24b4a5; */
-  /* box-sizing: content-box;
-  padding: 10px 370px 60px 100px; */
-  /* border-right-style: solid;
-  border-right-color: cornsilk;
-  border-right-width: 5px; */
-}
-
-img {
-  display: flex;
+  border: 1px solid green;
 }
 
 .last-container {
-
+  border: 1px solid blue;
   padding: 150px;
   margin: 30px;
   height: 100px;
-  width:600px;
+  width: 600px;
   align-items: center;
   justify-content: center;
   text-align: justify;
@@ -146,10 +125,11 @@ img {
 }
 
 .mid-container {
-   padding: 200px;
+  border: 1px solid yellow;
+  padding: 200px;
   margin: 30px;
   height: 100px;
-  width:600px;
+  width: 80%;
   align-items: center;
   justify-content: center;
   text-align: justify;
@@ -157,14 +137,14 @@ img {
   margin-left: auto;
 }
 
-.info {
-  /* border-right-style: solid;
-  border-right-color: cornsilk;
-  border-right-width: 5px; */
+.about-us {
+  background-color: white;
+  margin-top: 30px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  border: 1px solid rgb(236, 1, 197);
   padding: 30px;
-  margin: 30px;
   height: 100px;
-  width:600px;
+  width: 80%;
   align-items: center;
   justify-content: center;
   text-align: justify;
@@ -188,6 +168,7 @@ img {
 }
 
 .desc {
+  border: 1px solid red;
   font-style: normal;
   font-weight: 500;
   letter-spacing: 0.02em;
@@ -205,8 +186,8 @@ img {
   flex-direction: column; */
   /* align-items: center; */
   text-align: center;
-margin-left: auto;
-margin-right: auto
+  margin-left: auto;
+  margin-right: auto;
 }
 .account-sign {
   /* margin: 0px 10px 0px 60px; */
