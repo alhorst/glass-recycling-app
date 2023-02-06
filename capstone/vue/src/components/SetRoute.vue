e60f38d997cc57ee0e7d1b6bef53921b002d7b18
Matt Hosner <hosner@gmail.com>
2023-02-06 12:43:26 -0500

    Updated SetRoute.vue

---
+++ capstone/vue/src/components/SetRoute.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="map">
+      <h1>Route Map</h1>
+
+      
+
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: "SetRoute",
+    data() {
+
+    },
+
+    methods:{
+        directionsService() {
+            
+            this.map = new window.google.maps.Map(document.getElementById("map"), {
+    center: new window.google.maps.LatLng(40.46083373916581, -79.97458794232827),
+    zoom: 15,
+    mapTypeId: window.google.maps.MapTypeId.ROADMAP,
+});
+
+const directionsService = new window.google.maps.DirectionsService();
+
+directionsService.route(
+    {
+            origin: "3001 Railroad St, Pittsburgh, PA 15201",
+            destination: "3001 Railroad St, Pittsburgh, PA 15201",
+            waypoints: [
+          {
+            location: '4812 Broad St, Pittsburgh, PA 15244',
+            stopover: true
+          },{
+            location: '715 Ivy St, Pittsburgh, PA 15232',
+            stopover: true
+          }],
+
+            travelMode: "DRIVING",
+    },
+    (response, status) => {
+        if (status === "OK") {
+
+            new window.google.maps.DirectionsRenderer({
+                suppressMarkers: true,
+                directions: response,
+                
+              });
+        }
+    }
+)
+
+
+
+        }
+
+
+    }
+    
+  
+}
+</script>
+
+<style>
+
+</style>
